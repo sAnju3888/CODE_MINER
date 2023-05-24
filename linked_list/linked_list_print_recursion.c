1ae7c4fd8e16d1c9456d43c42daabbbb38909af6
sAnju3888 <shanjaikumar0001@gmail.com>
2023-05-24 10:14:01 +0530

    first commit on print recursion

---
+++ linked_list/linked_list_print_recursion.c
@@ -0,0 +1,25 @@
+void rec_print(struct Node* p){
+    if(p==NULL){
+        return ;
+    }
+    else{
+
+                printf("\n");
+        printf("%d sui",p->data);
+        rec_print(p->next);
+
+    }
+    
+}
+
+void rec_print_reverse(struct Node* p){
+    if(p==NULL){
+        return;
+    }else{
+        printf("\n");
+        rec_print_reverse(p->next);
+         printf("\n");
+         printf("%d dog",p->data);
+        
+    }
+}
